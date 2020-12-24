2e38f00bc8a60b4302dfd103791a58ef810f7933
Yaroslav <yaroslav.olex@gmail.com>
2020-12-24 10:05:14 +0200

    Initial Commit

---
+++ ListOfContacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListOfContacts
+//
+//  Created by Yaroslav on 24.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListOfContacts/ListOfContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfContactsApp.swift
+//  ListOfContacts
+//
+//  Created by Yaroslav on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ListOfContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
